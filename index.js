522c334708f41d13571f4957011b0e42848a84dc
Marcelo Luiz Onhate <onhate@gmail.com>
2024-03-01 18:59:14 -0300

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+const units = {
+  'ms': 'milliseconds',
+  'milliseconds': 'milliseconds',
+  's': 'seconds',
+  'sec': 'seconds',
+  'second': 'seconds',
+  'seconds': 'seconds',
+  'Second': 'seconds',
+  'Seconds': 'seconds',
+  'm': 'minutes',
+  'min': 'minutes',
+  'minute': 'minutes',
+  'minutes': 'minutes',
+  'Min': 'minutes',
+  'Minute': 'minutes',
+  'Minutes': 'minutes',
+  'h': 'hours',
+  'hr': 'hours',
+  'Hr': 'hours',
+  'hour': 'hours',
+  'hours': 'hours',
+  'Hour': 'hours',
+  'Hours': 'hours',
+  'd': 'days',
+  'D': 'days',
+  'day': 'days',
+  'days': 'days',
+  'Day': 'days',
+  'Days': 'days',
+  'w': 'weeks',
+  'W': 'weeks',
+  'wk': 'weeks',
+  'Wk': 'weeks',
+  'week': 'weeks',
+  'weeks': 'weeks',
+  'Week': 'weeks',
+  'Weeks': 'weeks',
+  'M': 'months',
+  'mon': 'months',
+  'month': 'months',
+  'months': 'months',
+  'Month': 'months',
+  'Months': 'months',
+  'y': 'years',
+  'Y': 'years',
+  'yr': 'years',
+  'yrs': 'years',
+  'year': 'years',
+  'years': 'years',
+  'Year': 'years',
+  'Years': 'years',
+  'q': 'quarters',
+  'qr': 'quarters',
+  'Q': 'quarters',
+  'Qr': 'quarters',
+  'qtr': 'quarters',
+  'quarter': 'quarters',
+  'quarters': 'quarters',
+  'Quarter': 'quarters',
+  'Quarters': 'quarters'
+};
+
+const unitToMilliseconds = {
+  'milliseconds': 1,
+  'seconds': 1000,
+  'minutes': 1000 * 60,
+  'hours': 1000 * 60 * 60,
+  'days': 1000 * 60 * 60 * 24,
+  'weeks': 1000 * 60 * 60 * 24 * 7,
+  'months': 1000 * 60 * 60 * 24 * 30, // Approximation
+  'years': 1000 * 60 * 60 * 24 * 365, // Approximation
+  'quarters': 1000 * 60 * 60 * 24 * 365 / 4 // Approximation
+};
+
+const parseableUnits = Object.keys(units).join('|');
+const regExp = new RegExp(`([-+])?(\\d*)\\s?(${parseableUnits})$`);
+
+/**
+ * Checks if a time expression is valid
+ * @param expression {string} - The time expression to check
+ * @return {boolean}
+ */
+export function isValid(expression) {
+  return regExp.test(expression);
+}
+
+/**
+ * Converts a time expression to a Date object
+ * @param expression {string} - The time expression to convert
+ * @param currentDate {Date} - The current date to use as a reference
+ * @return {Date|null}
+ */
+export function relativeToDate(expression, currentDate = new Date()) {
+  if (typeof expression !== 'string') {
+    throw new TypeError('The expression should be a string');
+  }
+
+  if (!isValid(expression)) {
+    throw new Error('The expression is not a valid expression');
+  }
+
+  const match = expression.match(regExp);
+  const operator = match[1] === '-' ? -1 : 1;
+  const amount = parseInt(match[2]);
+  const unit = units[match[3]];
+  const milliseconds = amount * unitToMilliseconds[unit];
+
+  const time = currentDate.getTime() + (operator * milliseconds);
+  return new Date(time);
+}
---
+++ index.test.js
@@ -0,0 +1,118 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+import { relativeToDate } from './index.js';
+
+describe('datelative', () => {
+  describe('Invalid expressions', () => {
+    it('should throw an error if the expression is not a string', () => {
+      assert.throws(() => relativeToDate(2), {
+        name: 'TypeError',
+        message: 'The expression should be a string'
+      });
+    });
+
+    it('should throw an error if the expression is not a valid expression', () => {
+      assert.throws(() => relativeToDate('2'), {
+        name: 'Error',
+        message: 'The expression is not a valid expression'
+      });
+    });
+  });
+
+  describe('Valid expressions', () => {
+    [
+      ['+2 days', 2 * 24 * 60 * 60 * 1000],
+      ['-2 days', -2 * 24 * 60 * 60 * 1000],
+      ['+2 weeks', 2 * 7 * 24 * 60 * 60 * 1000],
+      ['-2 weeks', -2 * 7 * 24 * 60 * 60 * 1000],
+      ['+2 months', 2 * 30 * 24 * 60 * 60 * 1000],
+      ['-2 months', -2 * 30 * 24 * 60 * 60 * 1000],
+      ['+2 years', 2 * 365 * 24 * 60 * 60 * 1000],
+      ['-2 years', -2 * 365 * 24 * 60 * 60 * 1000],
+      ['+2 quarters', 2 * 365 / 4 * 24 * 60 * 60 * 1000],
+      ['-2 quarters', -2 * 365 / 4 * 24 * 60 * 60 * 1000],
+      ['+2 milliseconds', 2],
+      ['-2 milliseconds', -2],
+      ['+2 seconds', 2 * 1000],
+      ['-2 seconds', -2 * 1000],
+      ['+2 minutes', 2 * 60 * 1000],
+      ['-2 minutes', -2 * 60 * 1000],
+      ['+2 hours', 2 * 60 * 60 * 1000],
+      ['-2 hours', -2 * 60 * 60 * 1000],
+
+      ['2 days', 2 * 24 * 60 * 60 * 1000],
+      ['2 weeks', 2 * 7 * 24 * 60 * 60 * 1000],
+      ['2 months', 2 * 30 * 24 * 60 * 60 * 1000],
+      ['2 years', 2 * 365 * 24 * 60 * 60 * 1000],
+      ['2 quarters', 2 * 365 / 4 * 24 * 60 * 60 * 1000],
+      ['2 milliseconds', 2],
+      ['2 seconds', 2 * 1000],
+      ['2 minutes', 2 * 60 * 1000],
+      ['2 hours', 2 * 60 * 60 * 1000],
+
+      ['+2days', 2 * 24 * 60 * 60 * 1000],
+      ['-2days', -2 * 24 * 60 * 60 * 1000],
+      ['+2weeks', 2 * 7 * 24 * 60 * 60 * 1000],
+      ['-2weeks', -2 * 7 * 24 * 60 * 60 * 1000],
+      ['+2months', 2 * 30 * 24 * 60 * 60 * 1000],
+      ['-2months', -2 * 30 * 24 * 60 * 60 * 1000],
+      ['+2years', 2 * 365 * 24 * 60 * 60 * 1000],
+      ['-2years', -2 * 365 * 24 * 60 * 60 * 1000],
+      ['+2quarters', 2 * 365 / 4 * 24 * 60 * 60 * 1000],
+      ['-2quarters', -2 * 365 / 4 * 24 * 60 * 60 * 1000],
+      ['+2milliseconds', 2],
+      ['-2milliseconds', -2],
+      ['+2seconds', 2 * 1000],
+      ['-2seconds', -2 * 1000],
+      ['+2minutes', 2 * 60 * 1000],
+      ['-2minutes', -2 * 60 * 1000],
+      ['+2hours', 2 * 60 * 60 * 1000],
+      ['-2hours', -2 * 60 * 60 * 1000],
+
+      // the other units
+      ['+2ms', 2],
+      ['-2ms', -2],
+      ['+2s', 2 * 1000],
+      ['-2s', -2 * 1000],
+      ['+2min', 2 * 60 * 1000],
+      ['-2min', -2 * 60 * 1000],
+      ['+2hr', 2 * 60 * 60 * 1000],
+      ['-2hr', -2 * 60 * 60 * 1000],
+      ['+2d', 2 * 24 * 60 * 60 * 1000],
+      ['-2d', -2 * 24 * 60 * 60 * 1000],
+      ['+2w', 2 * 7 * 24 * 60 * 60 * 1000],
+      ['-2w', -2 * 7 * 24 * 60 * 60 * 1000],
+      ['+2M', 2 * 30 * 24 * 60 * 60 * 1000],
+      ['-2M', -2 * 30 * 24 * 60 * 60 * 1000],
+      ['+2y', 2 * 365 * 24 * 60 * 60 * 1000],
+      ['-2y', -2 * 365 * 24 * 60 * 60 * 1000],
+      ['+2q', 2 * 365 / 4 * 24 * 60 * 60 * 1000],
+      ['-2q', -2 * 365 / 4 * 24 * 60 * 60 * 1000],
+
+      ['+2 ms', 2],
+      ['-2 ms', -2],
+      ['+2 s', 2 * 1000],
+      ['-2 s', -2 * 1000],
+      ['+2 min', 2 * 60 * 1000],
+      ['-2 min', -2 * 60 * 1000],
+      ['+2 hr', 2 * 60 * 60 * 1000],
+      ['-2 hr', -2 * 60 * 60 * 1000],
+      ['+2 d', 2 * 24 * 60 * 60 * 1000],
+      ['-2 d', -2 * 24 * 60 * 60 * 1000],
+      ['+2 w', 2 * 7 * 24 * 60 * 60 * 1000],
+      ['-2 w', -2 * 7 * 24 * 60 * 60 * 1000],
+      ['+2 M', 2 * 30 * 24 * 60 * 60 * 1000],
+      ['-2 M', -2 * 30 * 24 * 60 * 60 * 1000],
+      ['+2 y', 2 * 365 * 24 * 60 * 60 * 1000],
+      ['-2 y', -2 * 365 * 24 * 60 * 60 * 1000],
+      ['+2 q', 2 * 365 / 4 * 24 * 60 * 60 * 1000],
+      ['-2 q', -2 * 365 / 4 * 24 * 60 * 60 * 1000]
+    ].forEach(([expression, millis]) => {
+      it(expression, () => {
+        const reference = new Date();
+        const expected = new Date(reference.getTime() + millis);
+        assert.deepEqual(relativeToDate(expression, reference), expected);
+      });
+    });
+  });
+});
